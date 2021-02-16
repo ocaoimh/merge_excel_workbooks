ffcdf229a9227b3ea81e58702ba4ba7c7d414bf0
ocaoimh <okeeffe.cormac@gmail.com>
2021-02-16 10:24:25 +0100

    Update merge_workbooks.R

--- script/merge_workbooks.R
+++ script/merge_workbooks.R
@@ -1,9 +1,9 @@
 #this cleans up your global environment (optional)
 rm(list = ls())
 
-library(readxl)
-library(data.table)
-library(xlsx) 
+library(readxl) #this allows you import and extract data from your Excel workbooks
+library(data.table) #this library is the most crucial one and applies the functions that list and recombine the data
+library(xlsx) # this is optional. It's a tricky library that can break if you don't have the Java Development kit installed
          
 
 setwd("~/UXR/merge_excel_workbooks/my_data") 
@@ -15,13 +15,14 @@ file.list <- setNames(file.list, file.list)
 #takes file.list and read the data from the first worksheet of each workbook and copies this into our file.list
 df.list <- lapply(file.list, read_excel, sheet = 1)
 
-#take each workbook and join them together
-df.list <- Map(function(df, name) {
-  df$source_name <- name
-  df
+#take each workbook and join them together into a data table called 'all_files' 
+df.list <- Map(function(all_files, name) {
+  all_files$source_name <- name
+  all_files
 }, df.list, names(df.list))
-df <- rbindlist(df.list, idcol = "id", fill = TRUE)
+all_files <- rbindlist(df.list, idcol = "id", fill = TRUE)
 
+# uncomment the line below only if you installed "xlsx". It'll merge your workbooks into an excel file
 #write.xlsx(df, "group1.xlsx")
 
 # write your merged workbooks into one! 
